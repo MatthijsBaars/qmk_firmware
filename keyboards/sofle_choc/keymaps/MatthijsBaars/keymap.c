75469fb0466ce5f8ecd243fe9550989d17026f86
MatthijsBaars <matthijs.baars94@gmail.com>
2024-07-27 11:20:59 +0200

    own keymap

---
+++ keyboards/sofle_choc/keymaps/MatthijsBaars/keymap.c
@@ -0,0 +1,60 @@
+/* Copyright 2023 Brian Low
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Left-hand home row mods
+#define GUI_A LGUI_T(KC_A)
+#define ALT_R LALT_T(KC_R)
+#define SFT_S LSFT_T(KC_S)
+#define CTL_T LCTL_T(KC_T)
+
+// Right-hand home row mods
+#define CTL_N RCTL_T(KC_N)
+#define SFT_E RSFT_T(KC_E)
+#define ALT_I LALT_T(KC_I)
+#define GUI_O RGUI_T(KC_O)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/*
+ * COLMAK-DH
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |  =   |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  -   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | ESC  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   ;  |  \   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  |  '   |
+ * |------+------+------+------+------+------|  Mute |    | Pause |------+------+------+------+------+------|
+ * |  `   |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   /  |Option|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LEFT | RIGHT| DELE | /Bspc   /       \Space \  | Enter|  UP  | DOWN | Laye |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+
+LAYOUT(
+    KC_EQL,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,                       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_PMNS,
+    KC_ESC,  KC_Q,    KC_W,   KC_F,   KC_P,   KC_B,                       KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,KC_BSLS,
+    KC_TAB,  GUI_A,   ALT_R,  SFT_S,  CTL_T,  KC_G,                       KC_M,   CTL_N,  SFT_E,  ALT_I,  GUI_O,  KC_QUOT,
+    KC_GRV,  KC_Z,    KC_X,   KC_C,   KC_D,   KC_V,   KC_MUTE,   KC_MPLY, KC_K,   KC_H,   KC_COMM,KC_DOT, KC_SLSH,%,
+             KC_LGUI, KC_LEFT,KC_RGHT,KC_DEL, KC_BSPC,                    KC_SPC, KC_ENT, KC_UP,  KC_DOWN,%
+)
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
+};
+#endif
