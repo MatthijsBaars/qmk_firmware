0f2931059fe1755478877dbde43151ef8d239da7
MatthijsBaars <matthijs.baars94@gmail.com>
2024-08-10 19:33:04 +0200

    change rgb mod key positions. disable split detect, hopefully can now enter bios?

--- keyboards/sofle_choc/keymaps/MatthijsBaars/keymap.c
+++ keyboards/sofle_choc/keymaps/MatthijsBaars/keymap.c
@@ -99,13 +99,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_NUMPAD] = LAYOUT(
     //,------------------------------------------------.                    ,---------------------------------------------------.
-    XXXXXXX, RGB_TOG, RGB_VAD, RGB_VAI, RGB_MOD, RGB_M_P,                   XXXXXXX, KC_NUM, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, RGB_TOG, RGB_MOD, RGB_M_P, XXXXXXX,                   XXXXXXX, KC_NUM, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
-    _______, XXXXXXX, RGB_HUD, RGB_HUI, XXXXXXX, XXXXXXX,                   KC_CIRC, KC_P7,  KC_P8,   KC_P9,   KC_ASTR, XXXXXXX,
+    _______, XXXXXXX, RGB_VAD, RGB_VAI, XXXXXXX, XXXXXXX,                   KC_CIRC, KC_P7,  KC_P8,   KC_P9,   KC_ASTR, XXXXXXX,
     //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
-    XXXXXXX, XXXXXXX, RGB_SAD, RGB_SAI, XXXXXXX, XXXXXXX,                   KC_MINS, KC_P4,  KC_P5,   KC_P6,   KC_EQL,  KC_PIPE,
+    XXXXXXX, XXXXXXX, RGB_HUD, RGB_HUI, XXXXXXX, XXXXXXX,                   KC_MINS, KC_P4,  KC_P5,   KC_P6,   KC_EQL,  KC_PIPE,
     //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______,   _______,KC_PLUS, KC_P1,  KC_P2,   KC_P3,   KC_SLSH, TG(_NUMPAD),
+    XXXXXXX, XXXXXXX, RGB_SAD, RGB_SAI, XXXXXXX, XXXXXXX,_______,   _______,KC_PLUS, KC_P1,  KC_P2,   KC_P3,   KC_SLSH, TG(_NUMPAD),
     //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
                      _______, _______, _______, _______, _______,   _______, _______,  KC_P0,   KC_PDOT, _______
     //             \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[ ][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] =   { ENCODER_CCW_CW(KC_VOLU, KC_VOLD), ENCODER_CCW_CW(KC_PGDN, KC_PGUP)  },
     [1] =   { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(KC_WH_D, KC_WH_U)  },
-    [2] =   { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______)  }
+    [2] =   { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(KC_WH_D, KC_WH_U)  }
 };
 #endif
 
